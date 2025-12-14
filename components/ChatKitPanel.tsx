3d7ed154bcef9272924e8412898762e76bd5260a
ge-woll <ge-woll@t-online.de>
2025-12-14 11:58:56 +0100

    Update ChatKitPanel.tsx

--- components/ChatKitPanel.tsx
+++ components/ChatKitPanel.tsx
@@ -278,6 +278,10 @@ export function ChatKitPanel({
       attachments: {
         // Enable attachments
         enabled: false,
+        disclaimer: {
+      text: "Dies ist ein KI-System. Für generierte Inhalte wird keine Haftung übernommen.",
+      highContrast: true,
+    },
       },
       
     },
