1433f8278ff72de9d7de697deb6b0695d922776d
ge-woll <ge-woll@t-online.de>
2025-12-25 15:29:55 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </main>
 
         {/* Footer ganz unten */}
-        <footer className="bg-transparent text-center py-4 text-sm text-gray-700 dark:text-gray-300">
-          <div className="mt-2 flex flex-col sm:flex-row justify-center gap-2 sm:gap-4">
+       <footer className="bg-transparent text-center py-1 text-xs text-gray-600 dark:text-gray-400">
+        <div className="flex flex-col sm:flex-row justify-center gap-1 sm:gap-2">
             <Link href="/impressum" className="underline hover:text-blue-600">
               Impressum
             </Link>
